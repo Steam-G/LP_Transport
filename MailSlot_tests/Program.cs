1529564c4f11ae46c51dbe6978a9316f541f698b
l.andrey@npfgorizont.ru <l.andrey@npfgorizont.ru>
2020-02-04 11:08:14 +0500

    Проект TCP test теперь отслеживает в сети сигналы программы Регистрация, подключается и  нормально получает показания Забоя и Глубины долота. Так же в проекте появились некоторые наброски сетевых механизмов, таких как клиент-серверное общение и прием/получение сетевых пакетов. Плюс имеется набросок работы с данными в виде пакетов байт.

---
+++ MailSlot_tests/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSlot_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Handle 
+        }
+
+        /// <summary>
+        /// Create TransMailslotWrite request for client to write data to mailslot on server. 
+        /// </summary>
+        /// <param name = "messageId">the id of message, used to identity the request and the server response. </param>
+        /// <param name = "sessionUid">the valid session id, must be response by server of the session setup request. </param>
+        /// <param name = "treeId">the valid tree connect id, must be response by server of the tree connect. </param>
+        /// <param name = "flags">
+        /// The Flags field contains individual flags, as specified in [CIFS] sections 2.4.2 and 3.1.1. 
+        /// </param>
+        /// <param name = "flags2">
+        /// The Flags2 field contains individual bit flags that, depending on the negotiated SMB dialect, indicate   
+        /// various client and server capabilities. 
+        /// </param>
+        /// <param name = "mailslotName">The name of maislot to write to. </param>
+        /// <param name = "transactOptions">
+        /// A set of bit flags that alter the behavior of the requested operation. Unused bit fields MUST be set to  
+        /// zero by the client sending the request, and MUST be ignored by the server receiving the request. The 
+        /// client MAY set either or both of the following bit flags 
+        /// </param>
+        /// <param name = "timeOut">
+        /// The maximum amount of time in milliseconds to wait for the operation to complete. The client SHOULD set  
+        /// this to 0 to indicate that no time-out is given. If the operation does not complete within the specified  
+        /// time, the server MAY abort the request and send a failure response. 
+        /// </param>
+        /// <param name = "writeData">The data to write to mailslot. </param>
+        /// <param name = "priority">
+        /// This field MUST be in the range of 0 to 9. The larger value being the higher priority. 
+        /// </param>
+        /// <param name = "className">
+        /// The third setup word and the class of the mailslot request. This value MUST be set to one of the following 
+        /// values. 
+        /// </param>
+        /// <returns>a write mailslot request packet </returns>
+        private SmbTransMailslotWriteRequestPacket CreateTransMailslotWriteRequest(
+            ushort messageId,
+            ushort sessionUid,
+            ushort treeId,
+            SmbHeader_Flags_Values flags,
+            SmbHeader_Flags2_Values flags2,
+            string mailslotName,
+            TransSmbParametersFlags transactOptions,
+            uint timeOut,
+            byte[] writeData,
+            ushort priority,
+            SmbTransMailslotClass className)
+        {
+            if (mailslotName == null)
+            {
+                mailslotName = string.Empty;
+            }
+
+            SmbTransMailslotWriteRequestPacket packet = new SmbTransMailslotWriteRequestPacket();
+            packet.SmbHeader = CifsMessageUtils.CreateSmbHeader(SmbCommand.SMB_COM_TRANSACTION,
+                messageId, sessionUid, treeId, (SmbFlags)flags, (SmbFlags2)flags2);
+
+            // Set Smb Parameters
+            SMB_COM_TRANSACTION_Request_SMB_Parameters smbParameters =
+                new SMB_COM_TRANSACTION_Request_SMB_Parameters();
+            smbParameters.MaxParameterCount = this.capability.MaxParameterCount;
+            smbParameters.MaxDataCount = this.capability.MaxDataCount;
+            smbParameters.MaxSetupCount = this.capability.MaxSetupCount;
+            smbParameters.Flags = transactOptions;
+            smbParameters.Timeout = timeOut;
+            smbParameters.SetupCount = 3; // the correct count in word of the Setup is always 3.
+            smbParameters.Setup = new ushort[3];
+            smbParameters.Setup[0] = (ushort)TransSubCommand.TRANS_MAILSLOT_WRITE;
+            smbParameters.Setup[1] = (ushort)priority;
+            smbParameters.Setup[2] = (ushort)className;
+            smbParameters.WordCount = (byte)(CifsMessageUtils.GetSize<SMB_COM_TRANSACTION_Request_SMB_Parameters>(
+                smbParameters) / SmbCapability.NUM_BYTES_OF_WORD);
+
+            // Set Smb Data
+            SMB_COM_TRANSACTION_Request_SMB_Data smbData = new SMB_COM_TRANSACTION_Request_SMB_Data();
+            smbData.Name = CifsMessageUtils.ToSmbStringBytes(mailslotName, this.capability.IsUnicode);
+            smbData.Trans_Data = writeData;
+
+            packet.SmbParameters = smbParameters;
+            packet.SmbData = smbData;
+            packet.UpdateCountAndOffset();
+
+            // update TransactionSubCommand in SmbGlobalContext, record the transact action
+            this.capability.TransactionSubCommand = TransSubCommandExtended.TRANS_EXT_MAILSLOT_WRITE;
+
+            return packet;
+        }
+    }
+}
---
+++ TCP test/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_test
+{
+    class Program
+    {
+        
+        private const string server = "192.168.1.82";
+
+        static void Main(string[] args)
+        {
+            int port = 138;
+            UdpClient server = null;
+            try
+            {
+                server = new UdpClient(port);
+                // Создаем переменную IPEndPoint, чтобы передать ссылку на нее в Receive()
+                IPEndPoint remoteEP = null;
+                // Получаем и отдаем сразу. Эхо сервер
+                while (true)
+                {
+                    byte[] bytes = server.Receive(ref remoteEP);
+                    //server.Send(bytes, bytes.Length, remoteEP);
+                    string results = Encoding.UTF8.GetString(bytes);
+
+                    string subString = @"\MAILSLOT\chromatograph";
+                    int indexOfSubstring = results.IndexOf(subString); // равно 6
+
+                    if (indexOfSubstring > 0)
+                    {
+                        Console.WriteLine("ip: {0}, offset: {1}", remoteEP.Address, indexOfSubstring);
+                        string ipServer = remoteEP.Address.ToString();
+                        tcpClientReadPacket(ipServer);
+                    }
+
+                    
+                    //Console.WriteLine(remoteEP.ToString() + " отправил:" + results);
+                    if (results.ToLower().Equals("stop server")) break;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                if (server != null) server.Close();
+            }
+
+
+
+            //tcpClientReadPacket();
+        }
+
+        private static void tcpClientReadPacket(string ip)
+        {
+            const int port = 65004;
+
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(ip, port);
+
+                byte[] data = new byte[1514];
+                StringBuilder response = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buf = new byte[4096];
+                int i = 0; // это номер пакета данных, регистрация шлет их несколькими пачками
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    response.Append(Encoding.Default.GetString(data, 0, bytes));
+                    //response.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                    //response.Append(Encoding.Unicode.GetString(data, 0, bytes));
+
+                    i++;
+                    if (i == 2) // интересует второй пакет, там расположены забой и долото
+                    {
+                        string subString = @"UDataStorage";
+                        int indexOfSubstring = response.ToString().IndexOf(subString); // равно 6
+                        // Convert byte array elements to double values.
+                        Console.WriteLine(indexOfSubstring);
+                        BAToDouble(data, indexOfSubstring + 19 - 1514); // попал, поправить отображение числа
+                        BAToDouble(data, indexOfSubstring + 19 + 8 - 1514);
+                    }
+                }
+                while (stream.DataAvailable); // пока данные есть в потоке
+
+                //Выводим в консоли ответ от сервера в представленной кодировке
+                //Console.WriteLine(response.ToString());
+
+                // Закрываем потоки
+                stream.Close();
+                client.Close();
+
+                //WriteMultiLineByteArray(data);
+
+                //Console.WriteLine(formatter, "index", "array elements", "double");
+                //Console.WriteLine(formatter, "-----", "--------------", "------");
+
+                //// Convert byte array elements to double values.
+                //BAToDouble(data, 2120);
+                //BAToDouble(data, 2128);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+
+            //string msg = Exchange("192.168.1.45", 65004, "");
+
+
+            //Console.WriteLine(msg);
+
+            Console.WriteLine("Запрос завершен...");
+
+
+
+            //Console.Read();
+        }
+
+        static private string Exchange(string address, int port, string outMessage)
+        {
+            // Инициализация
+            TcpClient client = new TcpClient(address, port);
+            Byte[] data = Encoding.UTF8.GetBytes(outMessage);
+            NetworkStream stream = client.GetStream();
+            try
+            {
+                // Отправка сообщения
+                stream.Write(data, 0, data.Length);
+                // Получение ответа
+                Byte[] readingData = new Byte[256];
+                String responseData = String.Empty;
+                StringBuilder completeMessage = new StringBuilder();
+                int numberOfBytesRead = 0;
+                do
+                {
+                    numberOfBytesRead = stream.Read(readingData, 0, readingData.Length);
+                    completeMessage.AppendFormat("{0}", Encoding.UTF8.GetString(readingData, 0, numberOfBytesRead));
+                }
+                while (stream.DataAvailable);
+                responseData = completeMessage.ToString();
+                return responseData;
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
+        }
+
+
+        public static void ReceiveFrom1()
+        {
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 1280);
+            sock.Bind(iep);
+            EndPoint ep = (EndPoint)iep;
+            Console.WriteLine("Ready ...");
+
+            byte[] data = new byte[1024];
+            int recv = sock.ReceiveFrom(data, ref ep);
+            while (true)
+            {
+                string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("received: {0}  from: {1}", stringData, ep.ToString());
+            }
+
+        }
+
+
+
+
+
+        const string formatter = "{0,5}{1,27}{2,27:E16}";
+
+            // Convert eight byte array elements to a double and display it.
+            public static void BAToDouble(byte[] bytes, int index)
+            {
+                double value = BitConverter.ToDouble(bytes, index);
+
+            //Console.WriteLine(formatter, index,
+            //    BitConverter.ToString(bytes, index, 8), value);
+            Console.WriteLine(value.ToString());
+            }
+
+            // Display a byte array, using multiple lines if necessary.
+            public static void WriteMultiLineByteArray(byte[] bytes)
+            {
+                const int rowSize = 16;
+                int iter;
+
+                Console.WriteLine("initial byte array");
+                Console.WriteLine("------------------");
+
+                for (iter = 0; iter < bytes.Length - rowSize; iter += rowSize)
+                {
+                    Console.Write(
+                        BitConverter.ToString(bytes, iter, rowSize));
+                    Console.WriteLine("___" + iter.ToString());
+                }
+
+                Console.WriteLine(BitConverter.ToString(bytes, iter));
+                Console.WriteLine();
+            }
+        
+    }
+}
